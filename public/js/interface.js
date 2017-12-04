9ee5214a09cd2ef3c28ec816b0e263abd28ea6e3
Ignacio Panebianco <ignaciopanebianco@gmail.com>
2017-12-04 17:25:35 +0000

    Adds notes list (no links)

--- public/js/interface.js
+++ public/js/interface.js
@@ -4,11 +4,14 @@ window.addEventListener("load", function() {
 
   var createButton
   var notes = []
+  var linksList
 
   createButton = document.getElementById("create")
+  linksList = document.getElementById("links-list")
 
   createButton.addEventListener("click", function() {
     notes.push(new Note(document.getElementById("new-note").value))
-    console.log(notes)
+    console.log(notes.slice(-1)[0])
+    linksList.innerHTML += `<li><a>${notes.slice(-1)[0].getSummary()}</a></li>`
   })
 })
