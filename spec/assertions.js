c22bf0e0a255360caf860de3e0df202d50109719
Eustaquio122 <tiagofsousa@gmail.com>
2017-12-05 12:40:09 +0000

    Adds Assertions

---
+++ spec/assertions.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var assert = {
+    isTrue: function(assertionToCheck) {    
+        if (!assertionToCheck) {
+            throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
+        } else {
+            console.log("yay")
+        }
+    },
+
+    isFalse: function(assertionToCheck) {
+        if (assertionToCheck) {
+            throw new Error("Assertion failed: " + assertionToCheck + " is truthy");
+        } else {
+            console.log("yay")
+        }
+    },
+
+    returns: function(methodCall, expectedResult) {
+        return methodCall === expectedResult  
+    }
+};
