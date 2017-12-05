3ddf3436e53c431ba98ea2b4993e9ee85fbe9fb2
antonio <antoniobelmardacosta@gmail.com>
2017-12-05 15:02:54 +0000

    Create unit tests for Note model

---
+++ spec/noteSpec.js
@@ -0,0 +1,16 @@
+'use strict'
+
+function testNoteInstanceOfNote() {
+  var note = new Note("Ignacio");
+  return assert.isTrue(note instanceof Note);
+};
+
+function testGetTextReturnsTextProperty() {
+  var note = new Note("Ignacio");
+  return assert.returns(note.getText(), "Ignacio");
+}
+
+function testGetSummaryReturnsSummaryProperty() {
+  var note = new Note("Ignacio is the best at table tennis");
+  return assert.returns(note.getSummary(), "Ignacio is the best ...");
+}
